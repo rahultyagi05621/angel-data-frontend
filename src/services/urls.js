602aff15d7f06ab740d74bcab0e8f76672386327
Sahil Jaggarwal <108226778+sahiljaggarwal@users.noreply.github.com>
2024-07-20 15:54:50 +0530

    Update urls.js

--- src/services/urls.js
+++ src/services/urls.js
@@ -1,4 +1,5 @@
-export const baseUrl = "http://localhost:3001/user";
+// export const baseUrl = "http://localhost:3001/user";
+export const baseUrl = "https://angel-data-backend.onrender.com/user";
 export const loginUrl = `${baseUrl}/login`;
 export const signUpUrl = `${baseUrl}/signup`;
 export const contactUrl = `${baseUrl}/contact`;
