2b0b5020a710725a4efe040cd573c57c03c8ecda
rahultyagi05621 <Rahultyagi05621@gmail.com>
2024-08-05 16:49:58 +0530

    Update urls.js

--- src/services/urls.js
+++ src/services/urls.js
@@ -1,5 +1,7 @@
 // export const baseUrl = "http://localhost:3001/user";
-export const baseUrl = "https://angel-data-backend.onrender.com/user";
+// export const baseUrl = "https://angel-data-backend.onrender.com/user";
+ export const baseUrl = "https://angel-data-backend-01-1.onrender.com";
+
 export const loginUrl = `${baseUrl}/login`;
 export const signUpUrl = `${baseUrl}/signup`;
 export const contactUrl = `${baseUrl}/contact`;
